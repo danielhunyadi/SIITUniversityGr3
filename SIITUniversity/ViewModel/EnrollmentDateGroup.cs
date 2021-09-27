a000f74d8ae70c2ffe3e167ff72fb069fa21f694
Daniel Hunyadi <danyhunyadi@yahoo.com>
2021-09-27 20:17:08 +0300

    Add project files.

---
+++ SIITUniversity/ViewModel/EnrollmentDateGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SIITUniversity.ViewModel
+{
+	public class EnrollmentDateGroup
+	{
+		public DateTime? EnrollmentDate { get; set; }
+		public int StudentCount { get; set; }
+	}
+}
